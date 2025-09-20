c090964c3a9ad5b0afa32bdee6cf8d6bc5e1a45b
jerinzech <jerinzdev@gmail.com>
2025-09-20 23:14:43 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+hey!
+
+welcome to nmmd studio
+
+i am trying to create a website for my studio, fingers crossed!
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+function App() {
+
+  return (
+    <>
+      <div>nmmd studio</div>
+    </>
+  )
+}
+
+export default App
