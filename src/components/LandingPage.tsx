42215ce2aa6d5f1e1a0b416757453a9ff4272bf2
jerinzech <jerinzdev@gmail.com>
2025-09-21 00:04:21 +0200

    fix landing page layout

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+// import React from 'react'
 
 function LandingPage() {
   return (
-    <div id='landingpagetext'>we're nmmd studio</div>
+    <>
+
+      <div id='landingpagetext'>we're nmmd studio</div>
+
+      <div id='landingpagesubtext'>we are an indie design and dev studio</div>
+      <div id='bottomtext'>crafting with love from BLR and GNB</div>
+    </>
+
   )
 }
 
